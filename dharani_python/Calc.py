4b4b43f20cb38bb3e962a931db79f8964559d61f
GDharani19 <gottamdharani7626@gmail.com>
2024-06-20 14:40:33 +0530

    python

---
+++ dharani_python/Calc.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+import Calc
+a=10
+b=5
+print(Calc.add(a,b))
+print(Calc.sub(a,b))
+print(Calc.mul(a,b))
+print(Calc.div(a,b)) 
